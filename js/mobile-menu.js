6fa50cfcdd9cc7445abad73e68ced8e7ff8895a8
silvia80569 <157542289+silvia80569@users.noreply.github.com>
2025-05-01 23:23:26 +0200

    Restructure layout for mobile-first responsiveness

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
+  if (!mobileMenu || !openMenuBtn || !closeMenuBtn) return;
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
